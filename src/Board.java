b58224759f90aa98cb2ca4e58ea9c56a3477e50e
robertjlooby <robertjlooby@gmail.com>
2013-07-10 17:00:18 -0500

    Added row, column, and diagonal getters as well as the method to make moves.

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,48 @@
+import java.util.Arrays;
+
 public class Board {
-    private int[] state;
+    private int[][] state;
 
     public Board() {
-        state = new int[9];
+        state = new int[3][3];
+        for (int i = 0; i < state.length; i++) {
+            Arrays.fill(state[i], -1);
+        }
     }
 
-    public int[] getState() {
+    public int[][] getState() {
         return state;
+    }
+
+    public void makeMove(int row, int col, int player) {
+        state[row][col] = player;
+    }
+
+    public int[] getRow(int row) {
+        return state[row];
+    }
+
+    public int[] getColumn(int col) {
+        int[] column = new int[3];
+        for (int i = 0; i < column.length; i++) {
+            column[i] = state[i][col];
+        }
+        return column;
+    }
+
+    public int[] getForwardDiagonal() {
+        int[] diag = new int[3];
+        for (int i = 0; i < diag.length; i++) {
+            diag[i] = state[i][i];
+        }
+        return diag;
+    }
+
+    public int[] getBackwardDiagonal() {
+        int[] diag = new int[3];
+        for (int i = 0; i < diag.length; i++) {
+            diag[i] = state[i][diag.length - 1 - i];
+        }
+        return diag;
     }
 }
--- tests/BoardTest.java
+++ tests/BoardTest.java
@@ -5,15 +5,158 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class BoardTest {
     Board board;
+    int[][] boardState;
 
     @Before
     public void initialize() {
         board = new Board();
+        boardState = new int[][]{{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
     }
 
     @Test
-    public void shouldHaveAnEmptyArrayWhenConstructed() {
-        int[] arr = new int[9];
-        assertArrayEquals(arr, board.getState());
+    public void shouldHaveAnArrayOfNegativeOnesWhenConstructed() {
+        assertArrayEquals(boardState, board.getState());
+    }
+
+    @Test
+    public void shouldChangeCell_0_WhenAMoveIsMadeByPlayer_0() {
+        boardState[0][0] = 0;
+        board.makeMove(0, 0, 0);
+        assertArrayEquals(boardState, board.getState());
+    }
+
+    @Test
+    public void shouldChangeCell_0_WhenAMoveIsMadeByPlayer_1() {
+        boardState[0][0] = 1;
+        board.makeMove(0, 0, 1);
+        assertArrayEquals(boardState, board.getState());
+    }
+
+    @Test
+    public void shouldChangeCell_1_WhenAMoveIsMadeByPlayer_0() {
+        boardState[0][1] = 0;
+        board.makeMove(0, 1, 0);
+        assertArrayEquals(boardState, board.getState());
+    }
+
+    @Test
+    public void shouldChangeCell_8_WhenAMoveIsMadeByPlayer_0() {
+        boardState[2][2] = 0;
+        board.makeMove(2, 2, 0);
+        assertArrayEquals(boardState, board.getState());
+    }
+
+    @Test
+    public void shouldChangeCell_7_WhenAMoveIsMadeByPlayer_1() {
+        boardState[2][1] = 1;
+        board.makeMove(2, 1, 1);
+        assertArrayEquals(boardState, board.getState());
+    }
+
+    @Test
+    public void shouldChangeCell_048_WhenAMoveIsMadeByPlayer_010() {
+        boardState[0][0] = 0;
+        boardState[1][1] = 1;
+        boardState[2][2] = 0;
+        board.makeMove(0, 0, 0);
+        board.makeMove(1, 1, 1);
+        board.makeMove(2, 2, 0);
+        assertArrayEquals(boardState, board.getState());
+    }
+
+    @Test
+    public void shouldReturnTheFirstRow() {
+        assertArrayEquals(new int[]{-1, -1, -1}, board.getRow(0));
+    }
+
+    @Test
+    public void shouldReturnTheFirstRowAfterMove() {
+        board.makeMove(0, 1, 0);
+        assertArrayEquals(new int[]{-1, 0, -1}, board.getRow(0));
+    }
+
+    @Test
+    public void shouldReturnTheSecondRowAfterMove() {
+        board.makeMove(1, 1, 1);
+        assertArrayEquals(new int[]{-1, 1, -1}, board.getRow(1));
+    }
+
+    @Test
+    public void shouldReturnTheThirdRowAfterMove() {
+        board.makeMove(2, 0, 0);
+        board.makeMove(2, 2, 1);
+        assertArrayEquals(new int[]{0, -1, 1}, board.getRow(2));
+    }
+
+    @Test
+    public void shouldReturnTheFirstColumn() {
+        assertArrayEquals(new int[]{-1, -1, -1}, board.getColumn(0));
+    }
+
+    @Test
+    public void shouldReturnTheFirstColumnAfterMove() {
+        board.makeMove(1, 0, 0);
+        assertArrayEquals(new int[]{-1, 0, -1}, board.getColumn(0));
+    }
+
+    @Test
+    public void shouldReturnTheSecondColumnAfterMove() {
+        board.makeMove(1, 1, 1);
+        assertArrayEquals(new int[]{-1, 1, -1}, board.getColumn(1));
+    }
+
+    @Test
+    public void shouldReturnTheThirdColumnAfterMove() {
+        board.makeMove(0, 2, 0);
+        board.makeMove(2, 2, 1);
+        assertArrayEquals(new int[]{0, -1, 1}, board.getColumn(2));
+    }
+
+    @Test
+    public void shouldReturnTheForwardDiagonal() {
+        assertArrayEquals(new int[]{-1, -1, -1}, board.getForwardDiagonal());
+    }
+
+    @Test
+    public void shouldReturnTheForwardDiagonalAfterMove() {
+        board.makeMove(1, 1, 0);
+        assertArrayEquals(new int[]{-1, 0, -1}, board.getForwardDiagonal());
+    }
+
+    @Test
+    public void shouldReturnTheForwardDiagonalAfterAnotherMove() {
+        board.makeMove(1, 1, 1);
+        assertArrayEquals(new int[]{-1, 1, -1}, board.getForwardDiagonal());
+    }
+
+    @Test
+    public void shouldReturnTheForwardDiagonalAfterADifferentMove() {
+        board.makeMove(0, 0, 0);
+        board.makeMove(2, 2, 1);
+        assertArrayEquals(new int[]{0, -1, 1}, board.getForwardDiagonal());
+    }
+
+    @Test
+    public void shouldReturnTheBackwardDiagonal() {
+        assertArrayEquals(new int[]{-1, -1, -1}, board.getBackwardDiagonal());
+    }
+
+    @Test
+    public void shouldReturnTheBackwardDiagonalAfterMove() {
+        board.makeMove(1, 1, 0);
+        assertArrayEquals(new int[]{-1, 0, -1}, board.getBackwardDiagonal());
+    }
+
+    @Test
+    public void shouldReturnTheBackwardDiagonalAfterAnotherMove() {
+        board.makeMove(1, 1, 1);
+        assertArrayEquals(new int[]{-1, 1, -1}, board.getBackwardDiagonal());
+    }
+
+    @Test
+    public void shouldReturnTheBackwardDiagonalAfterADifferentMove() {
+        board.makeMove(0, 2, 0);
+        board.makeMove(2, 0, 1);
+        assertArrayEquals(new int[]{0, -1, 1}, board.getBackwardDiagonal());
     }
 }
