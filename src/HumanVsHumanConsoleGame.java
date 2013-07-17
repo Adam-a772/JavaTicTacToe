f12a01c0be621055e55062279126164b3a555d09
robertjlooby <robertjlooby@gmail.com>
2013-07-17 14:35:16 -0500

    Added script to play a human vs human console game.

---
+++ src/HumanVsHumanConsoleGame.java
@@ -0,0 +1,17 @@
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class HumanVsHumanConsoleGame {
+    public static void main(String[] args) {
+        ConsoleIO playerIO = new ConsoleIO(new InputStreamReader(System.in), new PrintWriter(System.out));
+        Player player0 = new HumanPlayer("X", playerIO);
+        Player player1 = new HumanPlayer("O", playerIO);
+        Game game = new Game(new Player[]{player0, player1}, new TicTacToeBoard(), new BoardConsoleIO(new PrintWriter(System.out)));
+        int winner = game.play();
+        if(winner > -1){
+            System.out.println("Player " + winner + " won!");
+        } else {
+            System.out.println("It was a tie.");
+        }
+    }
+}
