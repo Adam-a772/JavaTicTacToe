4f93f0089d0474674aa88c2968ad21069a7f812e
robertjlooby <robertjlooby@gmail.com>
2013-07-17 16:44:00 -0500

    AI is a little smarter. Now makes winning move if it can. Has helper methods for getting current player and making deep copies of arrays.

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class AIPlayer implements Player{
     private TicTacToeBoard board;
     private String symbol;
@@ -14,6 +16,11 @@ public class AIPlayer implements Player{
 
     @Override
     public int[] getMove(int[][] boardState) {
+        int currentPlayer = currentPlayer(boardState);
+        int[] winningMove = getWinningMove(boardState, currentPlayer);
+        if(winningMove[0] >= 0 && winningMove[1] >= 0){
+            return winningMove;
+        }
         for(int i = 0; i < boardState.length; i++){
             if(boardState[i][i] == -1){
                 return new int[]{i, i};
@@ -27,5 +34,44 @@ public class AIPlayer implements Player{
             }
         }
         return null;
+    }
+
+    private int[] getWinningMove(int[][] boardState, int currentPlayer) {
+        for(int row = 0; row < boardState.length; row++){
+            for(int col = 0; col < boardState.length; col++){
+                if(boardState[row][col] == -1){
+                    board.setState(deep2DArrayCopy(boardState));
+                    board.makeMove(row, col, currentPlayer);
+                    if(board.winner() == currentPlayer){
+                        return new int[]{row, col};
+                    }
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static int[][] deep2DArrayCopy(int[][] intArr){
+        int[][] copy = new int[intArr.length][];
+        for(int i = 0; i < intArr.length; i++){
+            copy[i] = Arrays.copyOf(intArr[i], intArr[i].length);
+        }
+        return copy;
+    }
+
+    public int currentPlayer(int[][] boardState) {
+        int[] counts = new int[]{0, 0};
+        for(int row = 0; row < boardState.length; row++){
+            for(int col = 0; col < boardState.length; col++){
+                if(boardState[row][col] > -1){
+                    ++counts[boardState[row][col]];
+                }
+            }
+        }
+        if(counts[0] > counts[1]){
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
--- tests/AIPlayerTest.java
+++ tests/AIPlayerTest.java
@@ -1,8 +1,11 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class AIPlayerTest {
     AIPlayer player;
@@ -29,5 +32,47 @@ public class AIPlayerTest {
     @Test
     public void shouldChooseCell4WhenOnlyCell0Taken(){
         assertArrayEquals(new int[]{1, 1}, player.getMove(new int[][]{{0, -1, -1}, {-1, -1, -1}, {-1, -1, -1}}));
+    }
+
+    @Test
+    public void shouldChooseLastCellWhenOnlyCellLeft(){
+        assertArrayEquals(new int[]{0, 0}, player.getMove(new int[][]{{-1, 1, 0}, {0, 1, 1}, {1, 0, 0}}));
+        assertArrayEquals(new int[]{2, 0}, player.getMove(new int[][]{{0, 1, 0}, {1, 1, 0}, {-1, 0, 1}}));
+        assertArrayEquals(new int[]{2, 2}, player.getMove(new int[][]{{0, 0, 1}, {1, 1, 0}, {0, 1, -1}}));
+        assertArrayEquals(new int[]{0, 2}, player.getMove(new int[][]{{1, 0, -1}, {0, 1, 1}, {0, 1, 0}}));
+        assertArrayEquals(new int[]{1, 2}, player.getMove(new int[][]{{1, 0, 1}, {1, 0, -1}, {0, 1, 0}}));
+        assertArrayEquals(new int[]{0, 1}, player.getMove(new int[][]{{1, -1, 0}, {0, 0, 1}, {1, 1, 0}}));
+        assertArrayEquals(new int[]{1, 0}, player.getMove(new int[][]{{0, 1, 0}, {-1, 0, 1}, {1, 0, 1}}));
+        assertArrayEquals(new int[]{2, 1}, player.getMove(new int[][]{{0, 1, 1}, {1, 0, 0}, {0, -1, 1}}));
+    }
+
+    @Test
+    public void shouldBeAbleToDetermineWhoseTurnItIs(){
+        assertEquals(0, player.currentPlayer(new int[][]{{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}}));
+        assertEquals(1, player.currentPlayer(new int[][]{{0, -1, -1}, {-1, -1, -1}, {-1, -1, -1}}));
+        assertEquals(0, player.currentPlayer(new int[][]{{0, -1, -1}, {-1, 1, -1}, {-1, -1, -1}}));
+        assertEquals(1, player.currentPlayer(new int[][]{{0, -1, -1}, {-1, 1, -1}, {-1, -1, 0}}));
+        assertEquals(0, player.currentPlayer(new int[][]{{0, -1, -1}, {1, 1, -1}, {-1, -1, 0}}));
+        assertEquals(1, player.currentPlayer(new int[][]{{0, -1, -1}, {1, 1, 0}, {-1, -1, 0}}));
+        assertEquals(0, player.currentPlayer(new int[][]{{0, -1, 1}, {1, 1, 0}, {-1, -1, 0}}));
+        assertEquals(1, player.currentPlayer(new int[][]{{0, -1, 1}, {1, 1, 0}, {0, -1, 0}}));
+        assertEquals(0, player.currentPlayer(new int[][]{{0, -1, 1}, {1, 1, 0}, {0, 1, 0}}));
+        assertEquals(1, player.currentPlayer(new int[][]{{0, 0, 1}, {1, 1, 0}, {0, 1, 0}}));
+    }
+
+    @Test
+    public void shouldPerformDeepCopy(){
+        int[][] intArr = new int[][]{{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
+        int[][] copy = AIPlayer.deep2DArrayCopy(intArr);
+        assertArrayEquals(intArr, copy);
+        assertNotSame(intArr[0], copy[0]);
+    }
+
+    @Test
+    public void shouldChooseWinningMove(){
+        assertArrayEquals(new int[]{2, 2}, player.getMove(new int[][]{{0, -1, 1}, {0, 0, 1}, {1, -1, -1}}));
+        assertArrayEquals(new int[]{0, 2}, player.getMove(new int[][]{{1, 1, -1}, {-1, 0, -1}, {0, 0, 1}}));
+        assertArrayEquals(new int[]{2, 0}, player.getMove(new int[][]{{1, 0, 0}, {-1, 0, -1}, {-1, 1, 1}}));
+
     }
 }
