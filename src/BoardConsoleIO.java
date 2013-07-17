afa643e96689cd1d13cb3ef57d8623b992cb18e2
robertjlooby <robertjlooby@gmail.com>
2013-07-17 10:49:20 -0500

    Refactored BoardConsoleIO into smaller methods.  Changed tests from unicode to regular characters to improve readability.

--- src/BoardConsoleIO.java
+++ src/BoardConsoleIO.java
@@ -3,26 +3,34 @@ import java.io.Writer;
 
 public class BoardConsoleIO {
     private PrintWriter outputStream;
-    private final static String horizontalDash = "\u2501";
-    private final static String verticalDash = "\u2503";
-    private final static String cross = "\u254B";
-    private final static String[] blank = new String[]{"   ", "   ", " "};
-    private final static String[] ex = new String[]{"\u2572 \u2571", " \u2573 ", "\u2571 \u2572"};
-    private final static String[] oh = new String[]{"\u256D\u2500\u256E", "\u2502 \u2502", "\u2570\u2500\u256F"};
+    private String horizontalDash;
+    private String verticalDash;
+    private String cross;
+    private String[] blank = new String[]{"   ", "   ", " "};
+    private String[] ex;
+    private String[] oh;
 
     public BoardConsoleIO(Writer writer) {
         outputStream = new PrintWriter(writer, true);
+        horizontalDash = "\u2501";
+        verticalDash = "\u2503";
+        cross = "\u254B";
+        ex = new String[]{"\u2572 \u2571", " \u2573 ", "\u2571 \u2572"};
+        oh = new String[]{"\u256D\u2500\u256E", "\u2502 \u2502", "\u2570\u2500\u256F"};
+    }
+
+    public BoardConsoleIO(Writer writer, String hDash, String vDash, String crss, String[] ex, String[] oh){
+        outputStream = new PrintWriter(writer, true);
+        horizontalDash = hDash;
+        verticalDash = vDash;
+        cross = crss;
+        this.ex = ex;
+        this.oh = oh;
     }
 
     public void printBoard(int[][] boardState, String zeroSymbol, String oneSymbol) {
         int size = boardState.length;
-        String horizontalDivider = "";
-        for (int column = 0; column < size; column++) {
-            if (column > 0) {
-                horizontalDivider += cross;
-            }
-            horizontalDivider += horizontalDash + horizontalDash + horizontalDash;
-        }
+        String horizontalDivider = constructHorizontalDivider(size);
         for (int row = 0; row < size; row++) {
             if (row > 0) {
                 outputStream.println(horizontalDivider);
@@ -31,44 +39,70 @@ public class BoardConsoleIO {
         }
     }
 
+    private String constructHorizontalDivider(int size) {
+        String horizontalDivider = "";
+        for (int column = 0; column < size; column++) {
+            if (column > 0) {
+                horizontalDivider += cross;
+            }
+            horizontalDivider += horizontalDash + horizontalDash + horizontalDash;
+        }
+        return horizontalDivider;
+    }
+
     public void printRow(int[] row, int rowNumber, String[] playerSymbols) {
-        String[][] playerTemplates = new String[2][];
-        if (playerSymbols[0].equals("X")) {
-            playerTemplates[0] = ex;
-        } else {
-            playerTemplates[0] = oh;
-        }
-        if (playerSymbols[1].equals("X")) {
-            playerTemplates[1] = ex;
-        } else {
-            playerTemplates[1] = oh;
-        }
+        String[][] playerTemplates = constructPlayerTemplates(playerSymbols);
         String[] rowStrings = new String[]{"", "", ""};
         int size = row.length;
         for (int column = 0; column < size; column++) {
             if (column > 0) {
-                for (int subRow = 0; subRow < 3; subRow++) {
-                    rowStrings[subRow] += verticalDash;
-                }
+                addDividerToCell(rowStrings);
             }
             if (row[column] >= 0) {
                 int player = row[column];
-                rowStrings[0] += playerTemplates[player][0];
-                rowStrings[1] += playerTemplates[player][1];
-                rowStrings[2] += playerTemplates[player][2];
+                String[] playerSymbol = playerTemplates[player];
+                addPlayerCellToRowStrings(rowStrings, playerSymbol);
             } else {
-                rowStrings[0] += blank[0];
-                rowStrings[1] += blank[1];
-                rowStrings[2] += blank[2];
                 String cellNum = Integer.toString(rowNumber * size + column);
-                if (cellNum.length() < 2) {
-                    rowStrings[2] += " ";
-                }
-                rowStrings[2] += cellNum;
+                addBlankCellToRowStrings(rowStrings, cellNum);
             }
         }
         for (int subRow = 0; subRow < 3; subRow++) {
             outputStream.println(rowStrings[subRow]);
         }
+    }
+
+    private void addBlankCellToRowStrings(String[] rowStrings, String cellNum) {
+        rowStrings[0] += blank[0];
+        rowStrings[1] += blank[1];
+        rowStrings[2] += blank[2];
+        if (cellNum.length() < 2) {
+            rowStrings[2] += " ";
+        }
+        rowStrings[2] += cellNum;
+    }
+
+    private void addPlayerCellToRowStrings(String[] rowStrings, String[] playerSymbol) {
+        for(int i = 0; i < rowStrings.length; i++){
+            rowStrings[i] += playerSymbol[i];
+        }
+    }
+
+    private void addDividerToCell(String[] rowStrings) {
+        for (int subRow = 0; subRow < 3; subRow++) {
+            rowStrings[subRow] += verticalDash;
+        }
+    }
+
+    private String[][] constructPlayerTemplates(String[] playerSymbols) {
+        String[][] playerTemplates = new String[2][];
+        for(int i = 0; i < playerSymbols.length; i++){
+            if (playerSymbols[i].equals("X")) {
+                playerTemplates[i] = ex;
+            } else {
+                playerTemplates[i] = oh;
+            }
+        }
+        return playerTemplates;
     }
 }
--- tests/BoardConsoleIOTest.java
+++ tests/BoardConsoleIOTest.java
@@ -13,136 +13,138 @@ public class BoardConsoleIOTest {
     @Before
     public void initialize() {
         ioOutput = new StringWriter();
-        io = new BoardConsoleIO(ioOutput);
+        String[] ex = new String[]{"\\ /", " x ", "/ \\"};
+        String[] oh = new String[]{"/-\\", "| |", "\\-/"};
+        io = new BoardConsoleIO(ioOutput, "-", "|", "+", ex, oh);
     }
 
     @Test
     public void shouldPrintAnEmptyFirstRow() {
-        String emptyRow = "   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \n" +
-                "  0\u2503  1\u2503  2\n";
+        String emptyRow = "   |   |   \n" +
+                          "   |   |   \n" +
+                          "  0|  1|  2\n";
         io.printRow(new int[]{-1, -1, -1}, 0, new String[]{"X", "O"});
         assertEquals(emptyRow, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAnEmptySecondRow() {
-        String emptyRow = "   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \n" +
-                "  3\u2503  4\u2503  5\n";
+        String emptyRow = "   |   |   \n" +
+                          "   |   |   \n" +
+                          "  3|  4|  5\n";
         io.printRow(new int[]{-1, -1, -1}, 1, new String[]{"X", "O"});
         assertEquals(emptyRow, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAnEmptyThirdRow() {
-        String emptyRow = "   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \n" +
-                "  6\u2503  7\u2503  8\n";
+        String emptyRow = "   |   |   \n" +
+                          "   |   |   \n" +
+                          "  6|  7|  8\n";
         io.printRow(new int[]{-1, -1, -1}, 2, new String[]{"X", "O"});
         assertEquals(emptyRow, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAnEmptyFirstRowFor4() {
-        String emptyRow = "   \u2503   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                "  0\u2503  1\u2503  2\u2503  3\n";
+        String emptyRow = "   |   |   |   \n" +
+                          "   |   |   |   \n" +
+                          "  0|  1|  2|  3\n";
         io.printRow(new int[]{-1, -1, -1, -1}, 0, new String[]{"X", "O"});
         assertEquals(emptyRow, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAnEmptyLastRowFor4() {
-        String emptyRow = "   \u2503   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                " 12\u2503 13\u2503 14\u2503 15\n";
+        String emptyRow = "   |   |   |   \n" +
+                          "   |   |   |   \n" +
+                          " 12| 13| 14| 15\n";
         io.printRow(new int[]{-1, -1, -1, -1}, 3, new String[]{"X", "O"});
         assertEquals(emptyRow, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAnEmptyBoard() {
-        String emptyBoard = "   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \n" +
-                "  0\u2503  1\u2503  2\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \n" +
-                "  3\u2503  4\u2503  5\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \n" +
-                "  6\u2503  7\u2503  8\n";
+        String emptyBoard = "   |   |   \n" +
+                            "   |   |   \n" +
+                            "  0|  1|  2\n" +
+                            "---+---+---\n" +
+                            "   |   |   \n" +
+                            "   |   |   \n" +
+                            "  3|  4|  5\n" +
+                            "---+---+---\n" +
+                            "   |   |   \n" +
+                            "   |   |   \n" +
+                            "  6|  7|  8\n";
         io.printBoard(new int[][]{{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}}, "X", "O");
         assertEquals(emptyBoard, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAnEmptyBoardFor4() {
-        String emptyBoard = "   \u2503   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                "  0\u2503  1\u2503  2\u2503  3\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                "  4\u2503  5\u2503  6\u2503  7\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                "  8\u2503  9\u2503 10\u2503 11\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                "   \u2503   \u2503   \u2503   \n" +
-                " 12\u2503 13\u2503 14\u2503 15\n";
+        String emptyBoard = "   |   |   |   \n" +
+                            "   |   |   |   \n" +
+                            "  0|  1|  2|  3\n" +
+                            "---+---+---+---\n" +
+                            "   |   |   |   \n" +
+                            "   |   |   |   \n" +
+                            "  4|  5|  6|  7\n" +
+                            "---+---+---+---\n" +
+                            "   |   |   |   \n" +
+                            "   |   |   |   \n" +
+                            "  8|  9| 10| 11\n" +
+                            "---+---+---+---\n" +
+                            "   |   |   |   \n" +
+                            "   |   |   |   \n" +
+                            " 12| 13| 14| 15\n";
         io.printBoard(new int[][]{{-1, -1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1}}, "X", "O");
         assertEquals(emptyBoard, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAFirstRowWithAnX() {
-        String row = "\u2572 \u2571\u2503   \u2503   \n" +
-                " \u2573 \u2503   \u2503   \n" +
-                "\u2571 \u2572\u2503  1\u2503  2\n";
+        String row = "\\ /|   |   \n" +
+                     " x |   |   \n" +
+                     "/ \\|  1|  2\n";
         io.printRow(new int[]{0, -1, -1}, 0, new String[]{"X", "O"});
         assertEquals(row, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAFirstRowWithAnO() {
-        String row = "\u256D\u2500\u256E\u2503   \u2503   \n" +
-                "\u2502 \u2502\u2503   \u2503   \n" +
-                "\u2570\u2500\u256F\u2503  1\u2503  2\n";
+        String row = "/-\\|   |   \n" +
+                     "| ||   |   \n" +
+                     "\\-/|  1|  2\n";
         io.printRow(new int[]{0, -1, -1}, 0, new String[]{"O", "X"});
         assertEquals(row, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintAFirstRowWithAnOForP2() {
-        String row = "\u256D\u2500\u256E\u2503   \u2503   \n" +
-                "\u2502 \u2502\u2503   \u2503   \n" +
-                "\u2570\u2500\u256F\u2503  1\u2503  2\n";
+        String row = "/-\\|   |   \n" +
+                     "| ||   |   \n" +
+                     "\\-/|  1|  2\n";
         io.printRow(new int[]{1, -1, -1}, 0, new String[]{"X", "O"});
         assertEquals(row, ioOutput.toString());
     }
 
     @Test
     public void shouldPrintFull4x4Board() {
-        String board = "\u2572 \u2571\u2503\u256D\u2500\u256E\u2503\u256D\u2500\u256E\u2503\u256D\u2500\u256E\n" +
-                " \u2573 \u2503\u2502 \u2502\u2503\u2502 \u2502\u2503\u2502 \u2502\n" +
-                "\u2571 \u2572\u2503\u2570\u2500\u256F\u2503\u2570\u2500\u256F\u2503\u2570\u2500\u256F\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "\u2572 \u2571\u2503\u2572 \u2571\u2503\u256D\u2500\u256E\u2503\u256D\u2500\u256E\n" +
-                " \u2573 \u2503 \u2573 \u2503\u2502 \u2502\u2503\u2502 \u2502\n" +
-                "\u2571 \u2572\u2503\u2571 \u2572\u2503\u2570\u2500\u256F\u2503\u2570\u2500\u256F\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "\u2572 \u2571\u2503\u2572 \u2571\u2503\u256D\u2500\u256E\u2503\u2572 \u2571\n" +
-                " \u2573 \u2503 \u2573 \u2503\u2502 \u2502\u2503 \u2573 \n" +
-                "\u2571 \u2572\u2503\u2571 \u2572\u2503\u2570\u2500\u256F\u2503\u2571 \u2572\n" +
-                "\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u254B\u2501\u2501\u2501\n" +
-                "\u256D\u2500\u256E\u2503\u2572 \u2571\u2503\u2572 \u2571\u2503\u256D\u2500\u256E\n" +
-                "\u2502 \u2502\u2503 \u2573 \u2503 \u2573 \u2503\u2502 \u2502\n" +
-                "\u2570\u2500\u256F\u2503\u2571 \u2572\u2503\u2571 \u2572\u2503\u2570\u2500\u256F\n";
+        String board = "\\ /|/-\\|/-\\|/-\\\n" +
+                       " x || ||| ||| |\n" +
+                       "/ \\|\\-/|\\-/|\\-/\n" +
+                       "---+---+---+---\n" +
+                       "\\ /|\\ /|/-\\|/-\\\n" +
+                       " x | x || ||| |\n" +
+                       "/ \\|/ \\|\\-/|\\-/\n" +
+                       "---+---+---+---\n" +
+                       "\\ /|\\ /|/-\\|\\ /\n" +
+                       " x | x || || x \n" +
+                       "/ \\|/ \\|\\-/|/ \\\n" +
+                       "---+---+---+---\n" +
+                       "/-\\|\\ /|\\ /|/-\\\n" +
+                       "| || x | x || |\n" +
+                       "\\-/|/ \\|/ \\|\\-/\n";
         io.printBoard(new int[][]{{0, 1, 1, 1}, {0, 0, 1, 1}, {0, 0, 1, 0}, {1, 0, 0, 1}}, "X", "O");
         assertEquals(board, ioOutput.toString());
     }
