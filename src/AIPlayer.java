3e5d8ed9bdab2b683c70d1e34a83bcda09113284
robertjlooby <robertjlooby@gmail.com>
2013-07-17 15:33:50 -0500

    Added minimal HumanvsAI. AI still very stupid.

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -14,7 +14,7 @@ public class AIPlayer implements Player{
 
     @Override
     public int[] getMove(int[][] boardState) {
-        for(int i = 0; i < 2; i++){
+        for(int i = 0; i < boardState.length; i++){
             if(boardState[i][i] == -1){
                 return new int[]{i, i};
             }
